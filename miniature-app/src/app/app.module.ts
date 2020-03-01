1044cd043825897003d3b6e1a883157c4143b1bd
Alex Shaw <alexandergshaw@gmail.com>
2020-03-01 10:32:45 -0600

    fix: solution to Angular Material module note found: imported components through secondary entry-points

--- miniature-app/src/app/app.module.ts
+++ miniature-app/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule,
-  MatIconModule,
-  MatCardModule,
-  MatButtonModule,
-  MatProgressSpinnerModule } from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+
 
 @NgModule({
   declarations: [
