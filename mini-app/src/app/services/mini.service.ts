f66954053c047a93843d0fb9b522537590a1e00d
Alex Shaw <alexandergshaw@gmail.com>
2020-06-09 20:32:33 -0500

    wip feat: implementing filtering for genders

--- mini-app/src/app/services/mini.service.ts
+++ mini-app/src/app/services/mini.service.ts
@@ -11,23 +11,47 @@ export class MiniService {
     this.minis = [
       {
         name: "gnome druid",
+        playerCharacter: true,
         imagePath: "assets/img/gnome_druid.jpg"
       },
       {
         name: "human druid",
+        playerCharacter: false,
         imagePath: "assets/img/human_druid.jpg"
       }
     ]
   }
 
-  filterMinis(searchTerm) {
+  searchMinis(searchTerm) {
     return this.minis.filter(mini => {
       return mini.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
     })
   }
+
+  filterPlayerCharacters() {
+    return this.minis.filter(mini => {
+      if(mini.hasOwnProperty('playerCharacter')) {
+        return mini.playerCharacter;
+      }
+    });
+  }
 }
 
 class Mini {
-  name: string;
+  name?: string;
+  race?: string;
+  gender?: string;
+  class?: string;
+  playerCharacter?: boolean;
+  nonPlayerCharacter?: boolean;
+
+  priceEstimate?: number;
+  company?: string;
+  miniatureLine?: string;
+  sku?: string;
+
+  baseSize?: number;
+  game?: string;
+
   imagePath: string;
 }
--- mini-app/src/app/tab1/tab1.page.ts
+++ mini-app/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MiniService } from '../services/mini.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
@@ -9,16 +10,26 @@ import { MiniService } from '../services/mini.service';
 export class Tab1Page implements OnInit  {
   public searchTerm: string = "";
   public minis: any;
+  genders;
+  genderForm;
 
-  constructor(private miniService: MiniService) {}
+  constructor(private miniService: MiniService) {
+    this.genderForm = new FormGroup({
+      "genders": new FormControl({value: 'male'})
+    });
+  }
 
   ngOnInit() {
-    this.setFilteredItems();
+    this.setSearchedMinis();
   }
  
-  setFilteredItems() {
-    console.log('in setFilteredItems');
-    this.minis = this.miniService.filterMinis(this.searchTerm);
+  setSearchedMinis() {
+    this.minis = this.miniService.searchMinis(this.searchTerm);
+  }
+
+  filterPlayerCharacters() {
+    console.log('in filterPlayerCharacters');
+    this.minis = this.miniService.filterPlayerCharacters();
     console.log('this.minis', this.minis);
 
   }
